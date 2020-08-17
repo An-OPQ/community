2a985386c2a76a2a9300bc43edc49349d401b8ec
Jerry <1503021658@qq.com>
2020-08-17 12:30:39 +0800

    更新

---
+++ src/main/resources/static/js/footer.html.js
@@ -0,0 +1,3 @@
+document.writeln("<div class=\'footer\' style=\'margin: 20px 0;color: #888;text-align: center;\'>");
+document.writeln("    Copyright &copy; 2017 ~ 2020");
+document.writeln("</div>");
