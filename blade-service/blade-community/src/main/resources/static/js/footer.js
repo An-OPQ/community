6cb459ef3f1ee792f696ba7dfccb8c091a315056
LiuXian <liuxian0728@foxmail.com>
2022-07-20 23:10:09 +0800

    update:分离前端项目

--- blade-service/blade-community/src/main/resources/static/js/footer.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<div class=\'\' style=\'margin-top: 20px;color: #888;text-align: center;background-color: #EFEFEF;height: 50px; width: 100%; line-height: 45px;\'>");
-document.writeln("Copyright &copy; 2020 ~ 2021");
-document.writeln("</div>");
